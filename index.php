68a142c2fe0ba2684e18bc0768e2467d267b7696
TueTran <bigbang159vn@gmail.com>
2025-12-17 20:29:17 +0700

    sưa tt sv

--- index.php
+++ index.php
@@ -46,8 +46,10 @@ $result = $conn->query("SELECT * FROM diem_thi ORDER BY id DESC");
 
     <div class="container">
         <div class="profile-info">
-            <h1>Hồ Sơ Sinh Viên</h1>
-            <p>Trần Thiên Tuệ - MSSV: <strong>DH52201727</strong></p>
+            <h1>Hồ Sơ Sinh Viên.</h1>
+            <p>Trần Thiên Tuệ. - MSSV: <strong>DH52201727.</strong></p>
+            <p>Lớp: <strong>D22_TH10.</strong></p>
+            <p>Trường: <strong>Đại Học Công Nghệ Sài Gòn (STU),</strong></p>
         </div>
 
         <?php echo $msg; ?>
